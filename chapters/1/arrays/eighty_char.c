bbb28cb2eb10d219284b4b0b04f874b8a58838af
asyks <aaron@aaronsykes.com>
2020-10-26 20:35:03 -0700

    Fix eighty_char

--- chapters/1/arrays/eighty_char.c
+++ chapters/1/arrays/eighty_char.c
@@ -2,52 +2,32 @@
 #define EIGHTY 80
 #define MAX 1000
 
-int getLine(char line[]);
-int copy(char to[], char from[], int spot);
+int countLineChars(void);
 
-/* print longest input line */
+/* count the number of lines over 80 characters */
 int main()
 {
-	int len;		/* curent line length */
-	int lines;		/* number of lines whose length is > 80 */
-	int spot;		/* the place in longest where the last entry ends */
-	char line[MAX];		/* current input line */
-  char longest[MAX];	/* longest line saved here */
-
-	while ((len = getLine(line)) > 0)
-		if (len > EIGHTY) {
-			++lines;
-			spot = copy(longest, line, spot);
+  int lineChars;
+  int linesOverEighty = 0;
+	while ((lineChars = countLineChars()) > 0) {
+		if (lineChars > EIGHTY) {
+			++linesOverEighty;
 		}
-	if (lines > 0)	/* there was a line */
-		printf("eighty character lines: %d\n %s \n", lines, longest);
+  }
+  printf("Number of eighty character lines: %d\n", linesOverEighty);
 
 	return 0;
 }
 
-/* getLine: read a line into s, return length */
-int getLine(char s[])
+int countLineChars()
 {
-	int c, i;
-
-	for (i=0; (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
-	if (c == '\n')	{
-		s[i] = c;
-		++i;
-	}
-	s[i] = '\0';
-	return i;
-}
-
-/* copy 'from' into 'to'; assume 'to' is big enough */
-int copy(char to[], char from[], int s)
-{
-	int i;
-
-	i = 0;
-	while ((to[s + i] = from[i]) != '\0')
-		++i;
-	s = s + i;
-	return s;
+	int c;
+  int i = 0;
+  while((c = getchar())) {
+    if (c == EOF || c == '\n') {
+      return i;
+    }
+    i++;
+  }
+  return 0;
 }
