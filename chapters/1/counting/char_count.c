1e1950b701e88979827be535667382e807bd8587
asyks <aaron@aaronsykes.com>
2020-10-28 20:48:48 -0700

    Fix char_count

--- chapters/1/counting/char_count.c
+++ chapters/1/counting/char_count.c
@@ -1,12 +1,8 @@
 # include <stdio.h>
 
-main()
-{
-
-	double nc;
-
-	for(nc = 0; getchar() != EOF; ++nc)
-		;
-	printf("%.0f\n", nc);
-
+int main() {
+  double nc;
+  for(nc = 0; getchar() != EOF; ++nc);
+  printf("%.0f\n", nc);
+  return 0;
 }
