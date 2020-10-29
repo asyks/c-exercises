a428c8ae472ac9687d276563c5d591dde9217cfe
asyks <aaron@aaronsykes.com>
2020-10-28 20:45:19 -0700

    Fix all_count

--- chapters/1/counting/all_count.c
+++ chapters/1/counting/all_count.c
@@ -1,21 +1,20 @@
 # include <stdio.h>
 
-main()
+/* count characters by type */
+int main()
 {
+  long c, ch, nl, t;
 
-	long c, ch, nl, t;
-
-	nl = 0;
-	t = 0;
-	for(ch = 0; (c = getchar()) != EOF; ++ch) {
-		if (c == '\n') {
-			++nl;
-		}
-		if (c == ' ') {
-			++t;
-		}
-	}
-
-	printf("characters:%d newlines:%d whitespace:%d \n", ch, nl, t);
-
+  nl = 0;
+  t = 0;
+  for(ch = 0; (c = getchar()) != EOF; ++ch) {
+    if (c == '\n') {
+      ++nl;
+    }
+    if (c == ' ') {
+      ++t;
+    }
+  }
+  printf("characters:%ld newlines:%ld whitespace:%ld \n", ch, nl, t);
+  return 0;
 }
