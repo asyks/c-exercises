b165fe6d39a8298709fb17d63977369b4b1ac82b
asyks <aaron@aaronsykes.com>
2020-10-29 20:49:13 -0700

    Fix functions exercises

--- chapters/1/functions/temp_func.c
+++ chapters/1/functions/temp_func.c
@@ -4,30 +4,21 @@ float to_celsius(float f);
 
 /* print Farenheit-Celsius table
 		for fahr = 0, 20, ..., 300 */
-
-main()
+int main()
 {
-
-	float fahr;
-	int lower, upper, step;
-
+  float fahr;
+  int lower = 0; 			/* lower limit of temperature table */
+  int upper = 300;		/* upper limit */
+  int step = 20;			/* step size */
   printf("%s \t %s \n","Fahrenheit","Celsius");
-
-	lower = 0; 			/* lower limit of temperature table */
-	upper = 300;		/* upper limit */
-	step = 20;			/* step size */
-
-	for (fahr = lower; fahr <= upper; fahr = fahr + step)
-		printf("%3.0f \t\t %6.1f \n", fahr, to_celsius(fahr));
-
+  for (fahr = lower; fahr <= upper; fahr = fahr + step) {
+    printf("%3.0f \t\t %6.1f \n", fahr, to_celsius(fahr));
+  }
+  return 0;
 }
 
 float to_celsius(float fahr)
 {
-
-	float celsius;
-
-	celsius =  (5.0/9.0) * (fahr-32.0);
-	return celsius;
-
+  float celsius =  (5.0/9.0) * (fahr-32.0);
+  return celsius;
 }
