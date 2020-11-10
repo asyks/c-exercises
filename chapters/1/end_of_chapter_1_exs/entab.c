0315fcb69c0eb9ac510a97cb16dc7739f038a170
asyks <aaron@aaronsykes.com>
2020-11-09 21:09:37 -0800

    Fix entab

--- chapters/1/end_of_chapter_1_exs/entab.c
+++ chapters/1/end_of_chapter_1_exs/entab.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
+
 #define TABSTOP 8 
 
-void count(void);
-
-main() {
-  int c;
-	count();
-	return 0;
-}
-
-void count() {
-	int i, c, n;
-	for (i=0; (c=getchar()) != EOF; ++i) {	
-		if (i >= 8) {
-			i = 1;
-		}
-		for (n=0; c == '\t' || c == ' '; ++i) { 
-			if (c == '\t') {
-				i += n += TABSTOP - i;	
-			}
-			if (c == ' ') {
-				n += 1;	
-			}
-			printf("%d",n);
-			c = getchar();
-		}
-		putchar(c);
-	}
-	printf("\n\0");
+/* Not sure why this one does what it does */
+int main() {
+  int i, c, n;
+  for (i=0; (c=getchar()) != EOF; ++i) {	
+    if (i >= 8) {
+      i = 1;
+    }
+    for (n=0; c == '\t' || c == ' '; ++i) { 
+      if (c == '\t') {
+        i += n += TABSTOP - i;	
+      }
+      if (c == ' ') {
+        n += 1;	
+      }
+      printf("%d",n);
+      c = getchar();
+    }
+    putchar(c);
+  }
+  printf("\n");
+  return 0;
 }
