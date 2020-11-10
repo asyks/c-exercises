f8b176808292c15360f526b71021951f5460ff3d
asyks <aaron@aaronsykes.com>
2020-11-09 19:24:08 -0800

    Fix end of chapter 1 exercise detab

--- chapters/1/end_of_chapter_1_exs/detab.c
+++ chapters/1/end_of_chapter_1_exs/detab.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-#define TABSTOP 8 
 
-void count(void);
+#define TABSTOP 8
 
-main() {
-  int c;
-	count();
-	return 0;
-}
-
-void count() {
-	int i, c, n;
-	for (i=0; (c=getchar()) != EOF; ++i) {	
-		if (c == '\t') {
+/* Replace tabs with spaces */
+int main() {
+  int i, c, n;
+  for (i=0; (c=getchar()) != EOF; ++i) {
+    if (c == '\t') {
       i = TABSTOP - i;
-			for (n=0; n <= i; n++) {
-				putchar(' ');
-			i = 0;
-			}
-		}
-		putchar(c);
-	}
-	printf("\n\0");
+      for (n=0; n <= i; n++) {
+        putchar(' ');
+      i = 0;
+      }
+    }
+    putchar(c);
+  }
+  printf("\n");
+  return 0;
 }
