e9b2adbfaf095700079ad81aea52408714e82fff
asyks <aaron@aaronsykes.com>
2020-11-25 21:27:59 -0800

    Get htoi working for short hexadecimal strings

--- chapters/2/htoi.c
+++ chapters/2/htoi.c
@@ -1,24 +1,62 @@
 #include <stdio.h>
 #include <ctype.h>
+
 #define MAX 100
 
-int isHexadecimal(char c);
+int hexArrayToInt(char hexArray[], int length);
+int isHexChar(char c);
+int charToInt(char c);
+long power (int base, int exponent);
 
 int main () {
   char c;
   char line[MAX];
+  int decimal;
   int n = 0;
-  while ((c=getchar()) && isHexadecimal(c)) {
+
+  while (isHexChar(c=getchar())) {
     line[n++] = c;
   }
-  line[n] = '\0';  /* Append null character so string is properly terminated */
-  printf("%s\n", line);
+
+  decimal = hexArrayToInt(line, n);
+
+  printf("%d\n", decimal);
   return 0;
 }
 
-int isHexadecimal (char c) {
+int charToInt(char c) {
+  if (isdigit(c)) {
+    return c - '0';
+  }
+  else {
+    return tolower(c) - 'a' + 10;
+  }
+}
+
+int hexArrayToInt (char hexArray[], int length) {
+  int index;
+  int decimal = 0;
+  int position = length - 1;
+
+  for (index = 0; index < length; index++) {
+    decimal += charToInt(hexArray[index]) * power(16, position--);
+  }
+  return decimal;
+}
+
+int isHexChar (char c) {
   if (isdigit(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')) {
     return 1;
   }
   return 0;
+}
+
+long power (int base, int exponent) {
+  long product = 1;
+
+  while (exponent > 0) {
+    product *= base;
+    --exponent;
+  }
+  return product;
 }
