20cb877be4769c0fbe0d1fb845136844c003c261
asyks <aaron@aaronsykes.com>
2020-11-26 20:14:45 -0800

    Rewrite invert

--- chapters/2/invert.c
+++ chapters/2/invert.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-int getbits(int x, int p, int n);
-
 int main(int argc, char *argv[]) {
-	int x,p,n,r;
-	if (argc) {
-		x = argv[1];
-		p = argv[2];
-		n = argv[3]
-		r = getbits(x,p,n);
-	}
-	printf("%d\n\0", r);	
-}
+  int numArgs;
+  int count;
 
-int getbits(int x,int p, int n) {
-	return (x >> (p+1-n)) & ~(~0 << n);
+  numArgs = argc - 1;
+
+  printf("%s called with %d args\n", argv[0], numArgs);
+
+  if (numArgs) {
+    for(count = numArgs; count > 0; count--) {
+      printf("arg %d: %s\n", count, argv[count]);
+    }
+  }
+  return 0;
 }
