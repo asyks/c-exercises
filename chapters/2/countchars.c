0142161cf6e7f51a6cc1c8a2675fd2bffef7caaa
asyks <aaron@aaronsykes.com>
2020-11-27 20:00:10 -0800

    Replace rewrite with countchars

---
+++ chapters/2/countchars.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define MAX 100
+#define TRUE 1
+#define FALSE 0
+
+int countChars();
+
+int main () {
+	int i;
+
+	i = countChars();
+	printf("%d\n", i);
+  return 0;
+}
+
+int countChars() {
+  int e = FALSE;
+  int i=0;
+  int c;
+  while (e == FALSE) {
+    if (i >= MAX || (c=getchar()) == EOF || c == '\n') {
+      e = TRUE;
+    }
+    else {
+      ++i;
+    }
+  }
+  return i;
+}
