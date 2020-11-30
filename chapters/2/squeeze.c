4ea1ef8c1db2921e82388bd37380fbb57e6cbfe2
asyks <aaron@aaronsykes.com>
2020-11-29 21:18:02 -0800

    Rewrite squeeze

--- chapters/2/squeeze.c
+++ chapters/2/squeeze.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
+
 #define MAX 100
+#define NULL_CHAR '\0'
 
-main() { 
-	char s1[MAX], s2[MAX];
-	int s2len;
-	s2len = countline(s2);
-  while (deletematch(s1, s2, s2len) != 0);
-	printf("%s\n\0", s1);
+void getString(char s[]);
+void excludeOccurrences(char s[], char t[]);
+
+int main() { 
+  char substring[MAX];
+  char string[MAX];
+
+  getString(substring);
+  getString(string);
+  excludeOccurrences(string, substring);
+
+  return 0;
 }
 
+void getString(char string[]) {
+  char character;
+  int index = 0;
 
-int countline(char s2[MAX]) {
-	int i, c;
-	for (i=0; (c=getchar()) != EOF && c != '\n'; ++i) {
-		s2[i] = c;	
-	}	
-	return i;
+  while ((character=getchar()) != EOF && character != '\n') {
+    string[index++] = character;	
+  }	
+  string[index] = NULL_CHAR;
 }
 
-int deletematch(char s1[MAX], char s2[MAX], int s2len) {
-	int i, c, n, match;
-	for (i=0; (c=getchar()) != EOF; ++i) {
-		match = 1;
-		for (n=0; n <= s2len && match; ++n) {	
-			if (c == s2[n]) {
-				match = 0;
-			}
-		}
-		if (match) {
-			s1[i] = c;
-		}
-	}
-	return 0;
+void excludeOccurrences(char string[], char subString[]) {
+  int indexString = 0;
+  int indexStringPrev = 0;
+  int indexSubString = 0;
+
+  while(string[indexString] != '\0') {
+    indexStringPrev = indexString; /* caputure the current index before attempting match */
+    while (string[indexString] == subString[indexSubString]) {
+      /* if the characters match, move both indexes to the next position */
+      indexString++;
+      indexSubString++;
+    }
+
+    if (subString[indexSubString] != NULL_CHAR) {
+      indexString = indexStringPrev; /* match not found, reset index */
+      putchar(string[indexString++]); /* print character at current index, and iterate */
+    }
+    indexSubString = 0;
+  }
+  putchar('\n');
 }
