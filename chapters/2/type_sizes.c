648fa9858538431c5714c5aec31779daeab48d91
asyks <aaron@aaronsykes.com>
2020-12-01 20:00:15 -0800

    Fix type_sizes

--- chapters/2/type_sizes.c
+++ chapters/2/type_sizes.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+
 #define MAX 10
 #define MIN 0
 
-main () {
-	const char c = 'a';
-	putchar(c);
+void test_int();
+
+int main () {
 	test_int();
+
+  return 0;
 }
 
-test_int() {
-	int i=MIN, c;
-	for (i; i <= MAX; ++i) {
-		printf("%d", i);
+void test_int() {
+	int i;
+
+	for (i=MIN; i <= MAX; ++i) {
+		printf("%d\n", i);
 	}
-	printf("\n\0");
 }
