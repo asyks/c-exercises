c47406a5144dd9a02ca80b1f5febb1b12f3be7dd
asyks <aaron@aaronsykes.com>
2020-12-02 20:27:02 -0800

    Fix chapter 3 itoa

--- chapters/3/itoa.c
+++ chapters/3/itoa.c
@@ -2,45 +2,45 @@
 #include <ctype.h>
 #include <string.h>
 
+int atoi(char s[]);
 void itoa(int n, char s[]);
-void getArgs(int argc, char argv[], int args[]);
 void reverse(char s[]);
 
-int main(int argc, int *argv[1]) {
-  int i, args[1000];
-	char s[1000];
-  /* getArgs(argc, *argv, args); */
-  /* i = args[1]; */
-	i = argv[1]
-  	printf("%d\n\0", i);
-  /* itoa(i, s); */
-  /* printf("%s", s); */
-	return 1;
+int main(int argc, char *argv[1]) {
+  int integer;
+  char string[100];
+
+  integer = atoi(argv[1]);
+  itoa(integer, string);
+  
+  printf("%s\n", string);
+  return 0;
 }
 
-void getArgs(int argc, char argv[], int args[]) {
-	int n;
-  char arg;
-	for (n = 0; n < argc; n++) {
-    if (isdigit((arg = argv[n]))) {
-			args[n] = arg;
-		}
-	}
+/* Casts character array of integer digits to its integer representation */
+int atoi(char s[]) {
+  int index;
+  int integer = 0;
+
+  for (index = 0; s[index] >= '0' && s[index] <= '9'; ++index) {
+    if (index == 0 && s[index] == '0') {
+      continue;
+    }
+    integer = 10 * integer + (s[index] - '0');
+  }
+  return integer;
 }
 
-void itoa(int n, char s[]) {
-	int i, sign;
+/* Casts integer to a character array of integer digits */
+void itoa(int integer, char string[]) {
+  int index = 0;
 
-	if ((sign = n) < 0)
-		n = -n;
-	i = 0;
-	do {
-		s[i++] = n % 10 + '0';
-	} while ((n /= 10) > 0);
-	if (sign < 0)
-		s[i++] = '-';
-	s[i] ='\0';
-	reverse(s);
+  do {
+    string[index++] = integer % 10 + '0';
+  } while ((integer /= 10) > 0);
+
+  string[index] ='\0';
+  reverse(string);
 }
 
 void reverse(char s[]) {
